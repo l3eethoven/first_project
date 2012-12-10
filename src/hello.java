61bc7a30eb0a541fbd1ac510cb4d6cf7aac62d4d
yourname <foo@email.com>
2012-12-10 20:06:47 +0700

    Improve check odd

--- src/hello.java
+++ src/hello.java
@@ -4,8 +4,13 @@ public static void main(String args[]){
 	System.out.println("Hello world");
 	int i;
 	
-	for (i=1;i<=2;i++){
-		System.out.println("i = "+i);
+	for (i=1;i<=100;i++){
+		System.out.print("Var i = "+i);
+		if (i%2==0){
+			System.out.println("<<<< This is Odd");
+			
+		}
+		else System.out.println("");
 		
 	}
 	
