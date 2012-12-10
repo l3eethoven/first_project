1785b1f0daa7625d681470a5aa260bbb80b788a2
yourname <foo@email.com>
2012-12-10 19:10:53 +0700

    2222

--- src/hello.java
+++ src/hello.java
@@ -1,8 +1,13 @@
 
 public class hello {
-public static void main(){
+public static void main(String args[]){
 	System.out.println("Hello world");
+	int i;
 	
+	for (i=1;i<=2;i++){
+		System.out.println("i = "+i);
+		
+	}
 	
 }
 }
